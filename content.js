8e4e1975ee4e2f66ebef7021195286e5b40f1d8e
Marius Brun <mariuabr@stud.ntnu.no>
2026-01-03 10:14:12 +0100

    Added another removal of irritating feature

--- content.js
+++ content.js
@@ -1,17 +1,29 @@
 function removeNetflixSansDiv() {
-const targetDiv = document.querySelector('div[data-no-focus-lock="true"]');
-if (targetDiv) {
-    console.log('Removing div:', targetDiv);
-    targetDiv.remove();
-}
+    const targetDiv = document.querySelector('div[data-no-focus-lock="true"]');
+    if (targetDiv) {
+        console.log('Removing Netflix popup div:', targetDiv);
+        targetDiv.remove();
+    }
 }
 
-removeNetflixSansDiv();
-console.log('DOM changed, checked for div.');
+function removeBtSoftwallDivs() {
+    const tarkovDivs = document.querySelectorAll('[id="bt-softwall"], .bt-softwall');
+    tarkovDivs.forEach(div => {
+        console.log('Removing bt-softwall div:', div);
+        div.remove();
+    });
+}
+
+function runRemovals() {
+    removeNetflixSansDiv();
+    removeBtSoftwallDivs();
+    console.log('DOM changed, checked for target divs.');
+}
+
+runRemovals();
 
 const observer = new MutationObserver(() => {
-removeNetflixSansDiv();
-console.log('DOM changed, checked for div.');
+    runRemovals();
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
